84c2aca2d4258bc50db178bd3320376f924c5f13
VicMonteiroo <v.aprigio@gsuite.iff.edu.br>
2025-07-17 13:58:03 -0300

    Add files via upload

---
+++ Atividade ppdm - post/app.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const app = express()
+const porta = 3000
+const rotaResultado = app.route("/resultado")
+app.use(express.urlencoded({extended: true}))
+
+app.set("view engine", "ejs")
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+rotaResultado.get((req,rest)=>{
+    res.redirect("/")
+})
+
+rotaResultado.post((req,res)=>{
+    let num1 = Number(req.body.num1)
+    let num2 = Number(req.body.num2)
+    let operacao = req.body.operacao
+    let total
+
+    if(operacao=="+"){
+        total = num1+num2
+    }
+    else if(operacao=="-"){
+        total = num1-num2
+    }
+    else if(operacao=="*"){
+        total = num1*num2
+    }
+    else if(operacao=="/"){
+        total = num1/num2
+    }
+    else{
+        res.render("/erro")
+    }
+    res.render("resultado",{total})
+})
+
+app.listen(porta)
